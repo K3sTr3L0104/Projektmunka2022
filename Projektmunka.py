b1dde15fa7ee25eda760efa7cb385ab191494904
K3sTr3L0104 <karakas.oliver.roland.2020i@bankitatabanya.hu>
2022-06-05 18:18:40 +0200

    asd

---
+++ Projektmunka.py
@@ -0,0 +1,9 @@
+#Projektmunka
+#Reszese: Nagy Eniko es Karakas Oliver Roland
+#Feladat: Fájl beolvasása, Fájlba írás, illetve függvények használata, írása.
+#Fájl megnyitás olvasásra (Nagy Enikő):
+
+
+
+
+#
