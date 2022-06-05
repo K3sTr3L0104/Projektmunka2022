1f47784192fc331f7a3109c5da6929fdfcccd360
K3sTr3L0104 <karakas.oliver.roland.2020i@bankitatabanya.hu>
2022-06-05 18:41:14 +0200

    asd

--- Projektmunka.py
+++ Projektmunka.py
@@ -1,9 +1,93 @@
 #Projektmunka
 #Reszese: Nagy Eniko es Karakas Oliver Roland
-#Feladat: Fájl beolvasása, Fájlba írás, illetve függvények használata, írása.
+#Feladat: LOTTÓ
 #Fájl megnyitás olvasásra (Nagy Enikő):
 
 
 
 
-#
+#Első sor kiíratása (Nagy Enikő):
+
+
+
+
+#Lottó számok generálása: (Karakas Roland):
+
+
+
+
+
+#Alap pénzösszeg beállítása (Nagy Enikő):
+
+
+
+
+
+
+#Lottószelvény választása, illetve annak az ára levonása (Karakas Roland):
+
+
+
+
+
+
+
+
+#Bekérni a felhasználó nevét, korát, születési évét (Nagy Enikő):
+
+
+
+
+
+
+
+#Ha nem elég idős, akkor ne engedje lottózni (Nagy Enikő):
+
+
+
+
+
+
+#Bekérni a felhasználótól 5 számot (Nagy Enikő):
+
+
+
+
+
+
+
+
+
+
+#Megnézni, hogy hány találata van (Karakas Roland, Nagy Enikő):
+
+
+
+
+
+
+
+
+#Feltételek,ciklusok létrehozása (Nagy Eniko, Karakas Roland):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Új fájlba íratni a versenyző nevét, és hogy hány számot talált el (Karakas Roland):
+
+
+
+
+
+#Az új fájból kiíratni a versenyző nevét, és a helyes találatokat, és hogy melyik lottószelvényt választotta (Karakas Roland, Nagy Enikő):
